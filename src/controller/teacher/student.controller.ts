2e96a30e011c663d111cb319e401f4f3197df4c3
huylg6*gis <huylg6*gis@fpt.com>
2025-01-13 15:26:52 +0700

    feat: get notification receipents

--- src/controller/teacher/student.controller.ts
+++ src/controller/teacher/student.controller.ts
@@ -1,10 +1,10 @@
-import { v4 } from 'uuid';
-import { User } from '../../entities/user.entity';
-import { AppDataSource } from '../../database/data-source';
-import { BUSINESS_MESSAGE, ROLE } from '../../constants';
-import { ApiResponseModel } from '../../utils/response.util';
 import { Request, Response } from 'express';
 import { In } from 'typeorm';
+import { v4 } from 'uuid';
+import { AppDataSource } from '../../database/data-source';
+import { User } from '../../entities/user.entity';
+import { BUSINESS_MESSAGE, ROLE } from '../../constants';
+import { ApiResponseModel } from '../../utils/response.util';
 
 const register = async (req: Request, res: Response): Promise<any> => {
 	const correlationId = v4();
@@ -78,16 +78,6 @@ const getCommonStudents = async (req: Request, res: Response): Promise<any> => {
 	}
 };
 
-function mapTeacherEntitiesToStudentList(data: User[]) {
-	if (data.length === 0) return [];
-
-	const studentList = data.flatMap((teacher) =>
-		teacher.students.map((student) => student.email)
-	);
-
-	return studentList;
-}
-
 const suspend = async (req: Request, res: Response): Promise<any> => {
 	const correlationId = v4();
 	try {
@@ -126,4 +116,79 @@ const suspend = async (req: Request, res: Response): Promise<any> => {
 	}
 };
 
-export default { register, getCommonStudents, suspend };
+const getNotificationReceipents = async (
+	req: Request,
+	res: Response
+): Promise<any> => {
+	const correlationId = v4();
+
+	try {
+		const { teacher, notification } = req.body;
+
+		const mentionedEmails = extractEmailsFromString(notification);
+
+		const userRepository = AppDataSource.getRepository(User);
+		const teacherEntity = await userRepository.findOne({
+			where: { email: teacher },
+			relations: ['students'],
+		});
+
+		if (!teacherEntity)
+			return ApiResponseModel.toBadRequest(
+				res,
+				BUSINESS_MESSAGE.INVALID_TEACHER,
+				correlationId
+			);
+
+		const registeredStudentEmails = teacherEntity.students
+			.filter((student) => !student.suspended)
+			.map((student) => student.email);
+
+		const allRecipients = Array.from(
+			new Set([...registeredStudentEmails, ...mentionedEmails])
+		);
+
+		const studentRepository = AppDataSource.getRepository(User);
+		const eligibleRecipients = await studentRepository.find({
+			where: {
+				email: In(allRecipients),
+				suspended: false,
+			},
+			select: ['email'],
+		});
+
+		const emails = eligibleRecipients.map((student) => student.email);
+
+		return ApiResponseModel.toSuccess(
+			res,
+			{ recipients: emails },
+			correlationId
+		);
+	} catch (error) {
+		return ApiResponseModel.toInternalServer(res, correlationId);
+	}
+};
+
+function mapTeacherEntitiesToStudentList(data: User[]) {
+	if (data.length === 0) return [];
+
+	const studentList = data.flatMap((teacher) =>
+		teacher.students.map((student) => student.email)
+	);
+
+	return studentList;
+}
+
+function extractEmailsFromString(value: string) {
+	return Array.from(
+		value.matchAll(/@([\w.+-]+@[\w-]+\.[\w.-]+)/g),
+		(m: string[]) => m[1]
+	);
+}
+
+export default {
+	register,
+	getCommonStudents,
+	suspend,
+	getNotificationReceipents,
+};
